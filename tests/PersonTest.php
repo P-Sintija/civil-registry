f1ad401cf1260709c9868effc69a78bf0c70383a
P-Sintija <sintija.putnina@gmail.com>
2021-04-12 09:31:59 +0300

    tests

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -8,7 +8,7 @@ class PersonTest extends TestCase
 {
     public function testName(): void
     {
-        $person = new Person('Anna','Beka','12345678901', 23, 'adrese');
+        $person = new Person(1,'Anna','Beka','12345678901', 23, 'adrese');
         $this->assertIsString($person->getName());
         $this->assertGreaterThanOrEqual(1,strlen($person->getName()));
         $this->assertLessThanOrEqual(255,strlen($person->getName()));
@@ -16,7 +16,7 @@ class PersonTest extends TestCase
 
     public function testSurname(): void
     {
-        $person = new Person('Anna','Beka','12345678901', 23, 'adrese');
+        $person = new Person(1,'Anna','Beka','12345678901', 23, 'adrese');
         $this->assertIsString($person->getSurname());
         $this->assertGreaterThanOrEqual(1,strlen($person->getSurname()));
         $this->assertLessThanOrEqual(255,strlen($person->getSurname()));
@@ -24,14 +24,14 @@ class PersonTest extends TestCase
 
     public function testPersonalId(): void
     {
-        $person = new Person('Anna','Beka','12345678901', 23, 'adrese');
+        $person = new Person(1,'Anna','Beka','12345678901', 23, 'adrese');
         $this->assertIsString($person->getPersonalId());
         $this->assertEquals(11,strlen($person->getPersonalId()));
     }
 
     public function testAge(): void
     {
-        $person = new Person('Anna','Beka','12345678901', 23, 'adrese');
+        $person = new Person(1,'Anna','Beka','12345678901', 23, 'adrese');
         $this->assertIsInt($person->getAge());
         $this->assertGreaterThanOrEqual(0, $person->getAge());
         $this->assertLessThan(110,$person->getAge());
--- tests/TokenTest.php
+++ tests/TokenTest.php
@@ -9,19 +9,18 @@ class TokenTest extends TestCase
 {
     public function testId(): void
     {
-        $person = new Person('Anna','Beka','12345678901', 23, 'adrese');
+        $person = new Person(1,'Anna','Beka','12345678901', 23, 'adrese');
         $token = new Token(
-            $person->getPersonalId(),
+            $person->getId(),
             password_hash($person->getPersonalId(), PASSWORD_BCRYPT),
             time()
         );
-        $this->assertEquals($person->getPersonalId(), $token->getPersonalId());
-        $this->assertEquals(11, strlen($token->getPersonalId()));
+        $this->assertEquals($person->getId(), $token->getId());
     }
 
     public function testKey(): void
     {
-        $person = new Person('Anna','Beka','12345678901', 23, 'adrese');
+        $person = new Person(1,'Anna','Beka','12345678901', 23, 'adrese');
         $token = new Token(
             $person->getPersonalId(),
             password_hash($person->getPersonalId(), PASSWORD_BCRYPT),
